bf49a5057473d88ca4af3a44706da5bb5df7753e
Nicolae Ciprian MAROSAN <marosa_a@etna-alternance.net>
2025-12-04 15:18:04 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/pathcraft-portfolio/', // 🔥 OBLIGATOIRE pour GitHub Pages
   plugins: [react()],
-})
+  base: "/pathcraft-portfolio/",  // ← obligatoire pour GitHub Pages
+});
